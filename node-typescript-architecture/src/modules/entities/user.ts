a34f0b159025f192027f62d4183b09024ab803a9
Kha Le <minhkhanb@gmail.com>
2023-06-27 11:19:08 +0700

    typeorm + mongodb

---
+++ node-typescript-architecture/src/modules/entities/user.ts
@@ -0,0 +1,13 @@
+import { Entity, ObjectId, ObjectIdColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @ObjectIdColumn()
+  id!: ObjectId;
+
+  @Column()
+  firstName!: string;
+
+  @Column()
+  lastName!: string;
+}
