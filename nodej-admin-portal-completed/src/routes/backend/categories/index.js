8deb567dc4dd58b43a55409f2b6bf9ab405b5d1f
Kha Le <minhkhanb@gmail.com>
2023-07-19 23:29:32 +0700

    init completed source

--- nodej-admin-portal-completed/src/routes/backend/categories/index.js
+++ nodej-admin-portal-completed/src/routes/backend/categories/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('backend/pages/items');
+  res.send('Category router');
 });
 
 module.exports = router;
--- nodej-admin-portal-completed/src/routes/backend/users/index.js
+++ nodej-admin-portal-completed/src/routes/backend/users/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('backend/pages/items');
+  res.send('User router');
 });
 
 module.exports = router;
